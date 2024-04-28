577562ddb41e7d05d66420d1a99a229890f85959
ckbiggs <146725018+ckbiggs@users.noreply.github.com>
2024-04-28 17:21:24 +0800

    modified:   index.css 	modified:   index.html 	modified:   index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 
-/* hamburger menu */
+//hamburger menu
 function myFunction() {
     var x = document.getElementById("navlink");
     if (x.style.display === "block") {
@@ -13,3 +13,12 @@ function myFunction() {
       x.style.display = "block";
     }
   }
+
+//button
+  function contactUs() {
+    var emailAddress = "recipient@example.com";
+    var subject = "Subject of the email";
+    var body = "Body of the email";
+    var mailtoLink = "mailto:" + encodeURIComponent(emailAddress) + "?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body);
+    window.location.href = mailtoLink;
+    }
