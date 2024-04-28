ce79c9bf05d1e6ac981bbe1d5b05d442b86ff1f2
Noirakumaa <hernandezlnathaniel@gmail.com>
2024-04-28 15:20:06 +0800

    modified:   index.css 	modified:   index.js

--- index.js
+++ index.js
@@ -2,3 +2,14 @@
 let vh = window.innerHeight * 0.01;
 
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+
+/* hamburger menu */
+function myFunction() {
+    var x = document.getElementById("navlink");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
