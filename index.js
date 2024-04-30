ca170eb609afa9672b60ef706b8a1a659cd0e78b
ckbiggs <146725018+ckbiggs@users.noreply.github.com>
2024-04-30 18:25:16 +0800

    overhauled html and css/added hamburger menu

--- index.js
+++ index.js
@@ -5,20 +5,24 @@ document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 
 //hamburger menu
-function myFunction() {
-    var x = document.getElementById("navlink");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
+const navEl = document.querySelector(".nav");
+const hamburgerEl = document.querySelector(".hamburger");
+
+hamburgerEl.addEventListener("click", () => {
+    navEl.classList.toggle("nav--open");
+    hamburgerEl.classList.toggle("hamburger--open");
+});
+
+navEl.addEventListener("click", () => {
+    navEl.classList.remove("nav--open");
+});
+
 
 //button
-  function contactUs() {
-    var emailAddress = "recipient@example.com";
-    var subject = "Subject of the email";
-    var body = "Body of the email";
-    var mailtoLink = "mailto:" + encodeURIComponent(emailAddress) + "?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body);
-    window.location.href = mailtoLink;
-    }
+function contactUs() {
+  var emailAddress = "recipient@example.com";
+  var subject = "Subject of the email";
+  var body = "Body of the email";
+  var mailtoLink = "mailto:" + encodeURIComponent(emailAddress) + "?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body);
+  window.location.href = mailtoLink;
+}
